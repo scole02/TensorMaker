746f529ebfe236d4f68e8d83db1a239148612967
scole02 <samcole107@gmail.com>
2022-12-13 17:32:13 -0800

    updated data template, training erroring out with dataloader

--- ImageClassDemo/core/views.py
+++ ImageClassDemo/core/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from io import BytesIO
 import base64
 from PIL import Image as im
+#from .forms import NameForm
 # from keras.applications import vgg16
 # from keras.applications.imagenet_utils import decode_predictions
 # from keras.preprocessing.image import img_to_array, load_img
@@ -19,10 +20,14 @@ import torch.backends.cudnn as cudnn
 import numpy as np
 import torchvision
 from torchvision import datasets, models, transforms
+from torch.utils.data import Dataset, DataLoader
 import matplotlib.pyplot as plt
+from skimage import io, transform
 import time
 import os
 import copy
+import mpld3
+from mpld3 import plugins, utils
 
 # Just normalization for validation
 data_transforms = {
@@ -46,13 +51,67 @@ def imshow(inp, title=None):
     std = np.array([0.229, 0.224, 0.225])
     inp = std * inp + mean
     inp = np.clip(inp, 0, 1)
-    plt.imshow(inp)
+    mpld3.imshow(inp)
     if title is not None:
         plt.title(title)
     plt.pause(0.001)  # pause a bit so that plots are updated
-    plt.imshow(inp)
+    mpld3.imshow(inp)
 
 def data(request):
+    if request.method == "POST":
+        #
+        # Django image API 
+        #
+        train_split = 0.8
+        # form = NameForm(request.POST)
+        files = request.FILES.getlist('classFiles1')
+        className = request.POST.get('classinput')
+        print(className)
+        # temp_file = dir[0].temporary_file_path
+        paths = [f.temporary_file_path() for f in files]
+        dirname = os.path.dirname(paths[0])
+        # make directory for class files
+        # os.mkdir(os.path.join(dirname, form.))
+
+        train_dataset = ImageDataset(paths[:int( len(paths) * train_split)])
+        val_dataset = ImageDataset(paths[int( len(paths) * (train_split)):])
+        print(f'len train:{len(train_dataset)} len val: {len(val_dataset)}')
+
+        train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=4,
+                                                     shuffle=True, num_workers=4)
+        val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=4,
+                                                     shuffle=True, num_workers=4)
+        dataloaders = {'train': train_dataloader, 'val': val_dataloader}
+        dataset_sizes = {'train' : len(train_dataset), 'test' : len(val_dataset)}
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+        model_ft = models.resnet18(pretrained=True)
+        num_ftrs = model_ft.fc.in_features
+        # Here the size of each output sample is set to 2.
+        # Alternatively, it can be generalized to nn.Linear(num_ftrs, len(class_names)).
+        model_ft.fc = nn.Linear(num_ftrs, 2)
+
+        model_ft = model_ft.to(device)
+
+        criterion = nn.CrossEntropyLoss()
+
+        # Observe that all parameters are being optimized
+        optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+
+        # Decay LR by a factor of 0.1 every 7 epochs
+        exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+        # print(train_dataset[0])
+        train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler, dataset_sizes, dataloaders, device)
+
+
+        # inp = out.numpy().transpose((1, 2, 0))
+        # mean = np.array([0.485, 0.456, 0.406])
+        # std = np.array([0.229, 0.224, 0.225])
+        # inp = std * inp + mean
+        # inp = np.clip(inp, 0, 1)
+        # data = im.fromarray(inp)
+        # data.save('test.png')
+
     return render(request, "data.html")
 
 def test(request):
@@ -95,3 +154,90 @@ def test(request):
         return render(request, "test.html")
     
     return render(request, "test.html")
+
+def train_model(model, criterion, optimizer, scheduler, dataset_sizes, dataloaders, device, num_epochs=25):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print(f'Epoch {epoch}/{num_epochs - 1}')
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+            if phase == 'train':
+                scheduler.step()
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        print()
+
+    time_elapsed = time.time() - since
+    print(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
+    print(f'Best val Acc: {best_acc:4f}')
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+
+
+
+class ImageDataset(Dataset):
+    # https://pytorch.org/tutorials/beginner/data_loading_tutorial.html#dataset-class
+    def __init__(self, filepaths: list):
+        """
+        Args:
+            filepaths[] (string): list of the images filepaths.
+        """
+        self.filepaths = filepaths
+
+    def __len__(self):
+        return len(self.filepaths)
+    
+    def __getitem__(self, idx):
+        img_name = self.filepaths[idx]
+        image = io.imread(img_name)
+        sample = {'image': image}
+
+        return sample
