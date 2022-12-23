b0b5789f7322b780e0330c341c820ddea6797c03
AviPeltz <aj.peltz@gmail.com>
2022-12-22 17:07:42 -0800

    Co-authored-by: scole02 <scole02@users.noreply.github.com>

--- modelmaker/trainer/urls.py
+++ modelmaker/trainer/urls.py
@@ -4,8 +4,11 @@ from django.conf.urls.static import static
 
 from . import views
 
+app_name = 'trainer'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('setup', views.setup_page, name='setup'),
     path('label', views.label, name='label'),
+    path('save_categories', views.save_categories, name='save_categories'),
 ]
